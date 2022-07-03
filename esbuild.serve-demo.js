13de3adebf77b983cceaa24fd7a3b2b263a6586c
tee-ck <quanyutian@outlook.com>
2022-07-03 20:36:51 +0800

    feat: added live-server reload for demo

---
+++ esbuild.serve-demo.js
@@ -0,0 +1,22 @@
+import LiveServer from "live-server";
+import chokidar from "chokidar";
+import {buildDemo} from "./esbuild.utils.js";
+
+await buildDemo();
+
+LiveServer.start({
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+    root: "./demo",
+    file: "index.html",
+    wait: 1000,
+    open: false,
+    logLevel: 2,
+});
+
+chokidar.watch(["./src", "./static"], {ignored:/build|node_modules|\.idea|\.git/}).on("all", async () => {
+    await buildDemo();
+
+    const {watcher} = LiveServer;
+    watcher.emit("change", "./demo/index.html");
+});
